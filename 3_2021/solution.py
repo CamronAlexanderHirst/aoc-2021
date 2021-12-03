9c8cbf989f1aa76f47e77a45f4b15cdd6e631548
CamronAlexanderHirst <camron.hirst@colorado.edu>
2021-12-03 16:42:20 -0700

    day 3 solution

--- 3_2021/solution.py
+++ 3_2021/solution.py
@@ -16,6 +16,8 @@ for line in lines:
     sum += line_split
 print(sum, num_lines)
 
+print('sum: {}'.format(sum))
+
 gamma = [int(i > num_lines/2) for i in sum]
 print('gamma: {}'.format(gamma))
 epsilon = [int(not i) for i in gamma]
@@ -33,21 +35,86 @@ print('solution: {}'.format(gamma_dec*epsilon_dec))
 # convert gamma, epsilon strings to binary numbers
 
 # %%
+# Part 2:
 
 # Oxygen generator rating:
 ary = [[int(num) for num in line] for line in lines]
-#print(ary.shape)
-
 pos = 0
 while len(ary) > 1:
-    if gamma[pos] == 1:
-        #for line in ary:
-        #    print(line)
-        #    if line[pos] == 1:
-        ary = [line for line in ary if int(line[pos]) == 1]
-    print(ary)
-    pos += 1
 
-print(ary)
+    # sum of pos-ith index in ary
+    sum = 0
+    for line in ary:
+        sum += int(line[pos])
+    
+    
+    if len(ary)%2 == 0.0 and float(sum) == len(ary)/2:
+        val = 1
+    elif sum > len(ary)/2:
+        val = 1
+    else:
+        val = 0
+
+    # find all lines that don't meet bit criteria
+    remove = []
+    for i in range(len(ary)):
+        line = ary[i]
+        if line[pos] != val:
+            remove.append(i)
+
+    # remove all lines that don't meet bit criteria
+    for i in sorted(remove, reverse=True):
+        ary.pop(i)
+
+    pos += 1
+    if pos > len(ary[0])-1:
+        break
+
+
+oxy = ary[0]
+oxy_bin = ''.join(map(str, oxy))
+oxy_dec = int(oxy_bin, base=2)
+
+
+# CO2 generator rating:
+ary = [[int(num) for num in line] for line in lines]
+pos = 0
+while len(ary) > 1:
+    # sum of pos-ith index in ary
+    sum = 0
+    for line in ary:
+        sum += int(line[pos])
+    
+    
+    if len(ary)%2 == 0.0 and float(sum) == len(ary)/2:
+        val = 1
+    elif sum > len(ary)/2:
+        val = 1
+    else:
+        val = 0
+
+    # find all lines that don't meet bit criteria
+    remove = []
+    for i in range(len(ary)):
+        line = ary[i]
+        if line[pos] == val:  # note changed logic
+            remove.append(i)
+    # remove all lines that don't meet bit criteria
+    for i in sorted(remove, reverse=True):
+        ary.pop(i)
+    pos += 1
+    if pos > len(ary[0])-1:
+        break
+
+co2 = ary[0]
+co2_bin = ''.join(map(str, co2))
+co2_dec = int(co2_bin, base=2)
+
+print('oxy_dec: {}'.format(oxy_dec))
+print('co2_dec: {}'.format(co2_dec))
+print('solution: {}'.format(oxy_dec*co2_dec))
+
+
+# %%
 
 # %%
